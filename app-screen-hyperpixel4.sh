8e5c07c76568f8f3897033fa2b5b54dfe8619dc4
Lex Maxwell <ox3e223c@gmail.com>
2020-08-15 03:58:57 +0800

    add func

--- app-screen-hyperpixel4.sh
+++ app-screen-hyperpixel4.sh
@@ -6,7 +6,10 @@ source ./app-global.sh
 OPTIONS=(
 1 "安裝 HyperPixel 4.0 Drivers"
 2 "安裝 Hyperpixel4 HDMI 自動切換功能"
-3 "移除自動切換"
+3 "移除 HDMI 自動切換"
+4 "安裝螢幕開關鍵"
+5 "0"
+6 "100"
 )
 
 CHOICE=$(dialog --clear \
@@ -25,18 +28,33 @@ case $CHOICE in
     ;;
   2)
     #備份檔案
-    #sudo cp /etc/rc.local ./screen/hyperpixel4/backup/etc/rc.local
-    #sudo cp /boot/config.txt ./screen/hyperpixel4/backup/boot/config.txt
+    sudo cp /etc/rc.local ./screen/hyperpixel4/backup/etc/rc.local
+    sudo cp /boot/config.txt ./screen/hyperpixel4/backup/boot/config.txt
 
     #安裝功能
-    #sudo cp /boot/config.txt /boot/hyper-config.txt
-    #sudo cp /boot/config.txt /boot/hdmi-config.txt
-    #sudo cp ./screen/hyperpixel4/boot/hdmi-config.txt /boot/hdmi-config.txt
+    sudo cp /boot/config.txt /boot/hyper-config.txt
+    sudo cp /boot/config.txt /boot/hdmi-config.txt
 
-    #sudo cp ./screen/hyperpixel4/etc/rc.local /etc/rc.local
+    sudo cp ./screen/hyperpixel4/boot/hdmi-config.txt /boot/hdmi-config.txt
+    sudo cp ./screen/hyperpixel4/etc/rc.local /etc/rc.local
     ;;
   3)
     #還原安裝
+    sudo cp ./screen/hyperpixel4/backup/etc/rc.local /etc/rc.local
+    sudo cp ./screen/hyperpixel4/backup/boot/config.txt /boot/config.txt
+    ;;
+  4)
+    #還原安裝
+    ;;
+  5)
+    gpio -g mode 19 out
+    gpio -g write 19 0
+    bash ./app-screen-hyperpixel4.sh
+    ;;
+  6)
+    gpio -g mode 19 out
+    gpio -g write 19 100 
+    bash ./app-screen-hyperpixel4.sh
     ;;
 esac
 
